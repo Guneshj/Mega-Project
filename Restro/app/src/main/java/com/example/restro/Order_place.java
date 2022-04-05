a00d405b63a11ccf3dabcd8f5d8eaef5b3fc7ee9
Guneshj <guneshjadhav11@gmail.com>
2022-04-05 14:48:25 +0530

    Restro folder added

---
+++ Restro/app/src/main/java/com/example/restro/Order_place.java
@@ -0,0 +1,15 @@
+package com.example.restro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Order_place extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_place);
+    }
+
+}
